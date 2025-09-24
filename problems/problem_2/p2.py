6fbaea438bc97f988dbab941c983f91adfbe62d0
ananyasammidi <ananyasammidi5@gmail.com>
2025-09-24 14:17:57 -0400

    intervals

--- problems/problem_2/p2.py
+++ problems/problem_2/p2.py
@@ -7,14 +7,14 @@ output: List of list of integers composing the covering.
 TODO: implement a correct greedy algorithm from the homework.
 '''
 def interval_covering(M: int, intervals: list) -> list:
-    intervals.sort(key=lambda x: (x[0], -x[1]))
-    covering = []
+    intervals = sorted(intervals, key=lambda x: x[1])
+    result = []
     ending = float("-inf")
 
     for start, end in intervals:
-        if end > ending:
-            covering.append([start, end])
+        if start >= ending:
+            result.append((start, end))
             ending = end
 
-    return covering
+    return result
     
