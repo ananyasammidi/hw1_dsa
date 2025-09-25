0927c36e7d35bc275d47967c3f19e8f40d545be7
ananyasammidi <ananyasammidi5@gmail.com>
2025-09-24 22:58:02 -0400

    challenge

--- challenge/challenge.py
+++ challenge/challenge.py
@@ -2,7 +2,10 @@ import math
 
 
 def reference_multiply(x, y):
-    pass
+    x1 = int("".join(str(i) for i in x), 2)
+    y1 = int("".join(str(i) for i in y), 2)
+    return x1 * y1
+    
 
 
 def karatsuba(x, y):
